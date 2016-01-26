2888e54b2d2605f3701bb3281239451b6006183a
Jinsung Lim <js99028@gmail.com>
2016-01-27 00:18:14 +0900

    Posting API updated
    Model:
    Filed "time" added to "Post" model.
    Model "Comment" added.
    
    API:
    Post API of "Post" created. Now posting a post is possible.

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask import Flask, render_template, request, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask.ext.restless import APIManager
+from flask.ext.restless import APIManager, ProcessingException
 from flask.ext.login import LoginManager, login_user, logout_user, current_user
 import json
+from datetime import datetime
 
 def add_cors_headers(response):
-	response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8100'
+	origin = request.headers.get('Origin')
+	response.headers['Access-Control-Allow-Origin'] = origin if origin in ['http://localhost:8100', 'file://', 'null'] else ''
 	response.headers['Access-Control-Allow-Credentials'] = 'true'
 	response.headers['Access-Control-Allow-Headers'] = 'Content-type'
 	# Set whatever other headers you like...
@@ -28,6 +30,10 @@ userPost = db.Table('user-post',
 	db.Column('user_id', db.String(8), db.ForeignKey('user.id'), nullable=False),
 	db.Column('post_id', db.Integer, db.ForeignKey('post.id'), nullable=False)
 )
+userComment = db.Table('user-comment',
+	db.Column('user_id', db.String(8), db.ForeignKey('user.id'), nullable=False),
+	db.Column('comment_id', db.Integer, db.ForeignKey('comment.id'), nullable=False)
+)
 postSubject = db.Table('post-subject',
 	db.Column('post_id', db.Integer, db.ForeignKey('post.id'), nullable=False),
 	db.Column('subject_id', db.Integer, db.ForeignKey('subject.id'), nullable=False)
@@ -42,6 +48,7 @@ class User(db.Model):
 	name = db.Column(db.String(), nullable=False)
 	password = db.Column(db.String(), nullable=False)
 	posts = db.relationship('Post', backref='op', lazy='dynamic')
+	comments = db.relationship('Comment', backref='cmter', lazy='dynamic')
 
 	def __init__(self, id, name, password):
 		self.id = id
@@ -73,10 +80,13 @@ class User(db.Model):
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	content = db.Column(db.String(120), nullable=False)
-	op_id = db.Column(db.String(8), db.ForeignKey('user.id'), nullable=False)
+	op_id = db.Column(db.String(8), db.ForeignKey('user.id'), default=lambda: current_user.id, nullable=False)
+	time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+	image = db.Column(db.String())
 	places = db.relationship('Place', secondary=postPlace, backref=db.backref('posts', lazy='dynamic'))
 	subjects = db.relationship('Subject', secondary=postSubject, backref=db.backref('posts', lazy='dynamic'))
-	people = db.relationship('User', secondary=userPost, backref=db.backref('tagged', lazy='dynamic'))
+	people = db.relationship('User', secondary=userPost, backref=db.backref('taggedPost', lazy='dynamic'))
+	comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
 class Place(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -86,8 +96,44 @@ class Subject(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(), unique=True, nullable=False)
 
+class Comment(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+	user_id = db.Column(db.String(8), db.ForeignKey('user.id'), nullable=False)
+	time = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+	content = db.Column(db.String(), nullable=False)
+	people = db.relationship('User', secondary=userComment, backref=db.backref('taggedComment', lazy='dynamic'))
+
+def postGetPost(item):
+	item['hasImage'] = True if item['image'] else False
+	diff = datetime.utcnow()-datetime.strptime(item['time'], '%Y-%m-%dT%H:%M:%S.%f')
+
+	year = int(diff.days/365)
+	month = int(diff.days/30)
+	day = int(diff.days)
+	hour = int(diff.seconds/3600)
+	minute = int(diff.seconds/60)
+	item['ago'] = ("%d year%s" % (year, 's' if year > 1 else '')) if year else ("%d month%s" % (month, 'es' if month > 1 else '')) if month else ("%d day%s" % (day, 's' if day > 1 else '')) if day else ("%d hour%s" % (hour, 's' if hour > 1 else '')) if hour else ("%d min%s" % (minute, 's' if minute > 1 else '')) if minute else "Just now"
+	item['now'] = not (year or month or day or hour) and minute < 6
+	return item
+
+def postGetSinglePost(result, **kw):
+	result = postGetPost(result)
+
+def postGetManyPost(result, **kw):
+	result['objects'] = list(map(postGetPost, result['objects']))
+
+def parsePosting(data, **kw):
+	if not current_user.is_authenticated:
+		raise ProcessingException(description='Login first!', code=401)
+
+	data['id'] = None
+
 manager.create_api(User, methods=['POST', 'DELETE'])
-manager.create_api(User, methods=['GET'], exclude_columns=['password'])
+manager.create_api(User, methods=['GET'], exclude_columns=['password', 'posts.op_id', 'posts.op.password'])
+
+manager.create_api(Post, methods=['GET'], exclude_columns=['op.password', 'op_id'], postprocessors={'GET_SINGLE': [postGetSinglePost], 'GET_MANY': [postGetManyPost]})
+manager.create_api(Post, methods=['POST'], include_columns=['id', 'content'], preprocessors={'POST': [parsePosting]})
 
 
 @login_manager.user_loader
@@ -123,6 +169,6 @@ def currentUser():
 		return abort(401)
 
 if __name__ == '__main__':
-	app.secret_key = 'super secret key'
+	app.secret_key = "N$KFgm*^wrKpN_x4$wnv#+vj-tU6Rh8Z"
 	app.config['SESSION_TYPE'] = 'filesystem'
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0')
